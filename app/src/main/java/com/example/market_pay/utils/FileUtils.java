d02bc18dda93cf498e07220cca159345ce7cb2c2
AyuMuhafilah <ayumuhafilah10@gmail.com>
2025-05-31 12:58:09 +0700

    -

--- app/src/main/java/com/example/market_pay/utils/FileUtils.java
+++ app/src/main/java/com/example/market_pay/utils/FileUtils.java
@@ -1,2 +1,24 @@
-package com.example.market_pay.utils;public class FileUtils {
+package com.example.market_pay.utils;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+
+import androidx.annotation.Nullable;
+
+public class FileUtils {
+    @Nullable
+    public static String getPath(Context context, Uri uri) {
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = context.getContentResolver().query(uri, projection, null, null, null);
+        if (cursor != null) {
+            int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            String filePath = cursor.getString(columnIndex);
+            cursor.close();
+            return filePath;
+        }
+        return null;
+    }
 }
--- app/src/main/java/com/example/market_pay/view/customer/DaftarMerchantActivity.java
+++ app/src/main/java/com/example/market_pay/view/customer/DaftarMerchantActivity.java
@@ -20,36 +20,19 @@ import com.example.market_pay.utils.TimePicker;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class DaftarMerchantActivity extends AppCompatActivity {
+
     private TextInputEditText editTextDate, pilihFile;
     private EditText editTextTimeBuka, editTextTimeTutup;
+    private ImageView back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daftar_merchant);
 
-        // Back
-        ImageView back = findViewById(R.id.iconBack);
-        back.setOnClickListener(v->{
-            finish();
-        });
-
-        // Pilih File
-        pilihFile = findViewById(R.id.pilihFile);
-        pilihFile.setOnClickListener(v -> {
-            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-            intent.setType("image/*"); // atau jenis file tertentu, contoh "image/*"
-            startActivityForResult(intent, 100);  // 100 adalah requestCode bebas
-        });
-
+        initViews();
+        setListeners();
         new WilayahHelper(this);
-        editTextDate = findViewById(R.id.editTextDate);
-        editTextDate.setOnClickListener(v -> DatePicker.showDatePicker(this, editTextDate));
-
-        editTextTimeBuka = findViewById(R.id.txtBuka);
-        editTextTimeBuka.setOnClickListener(v -> TimePicker.showTimePicker(this, editTextTimeBuka));
-        editTextTimeTutup = findViewById(R.id.txtTutup);
-        editTextTimeTutup.setOnClickListener(v -> TimePicker.showTimePicker(this, editTextTimeTutup));
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -58,31 +41,49 @@ public class DaftarMerchantActivity extends AppCompatActivity {
         });
     }
 
+    private void initViews() {
+        back = findViewById(R.id.iconBack);
+        pilihFile = findViewById(R.id.pilihFile);
+        editTextDate = findViewById(R.id.editTextDate);
+        editTextTimeBuka = findViewById(R.id.txtBuka);
+        editTextTimeTutup = findViewById(R.id.txtTutup);
+    }
+
+    private void setListeners() {
+        back.setOnClickListener(v -> finish());
+
+        pilihFile.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.setType("image/*");
+            startActivityForResult(intent, 100);
+        });
+
+        editTextDate.setOnClickListener(v -> DatePicker.showDatePicker(this, editTextDate));
+
+        editTextTimeBuka.setOnClickListener(v -> TimePicker.showTimePicker(this, editTextTimeBuka));
+        editTextTimeTutup.setOnClickListener(v -> TimePicker.showTimePicker(this, editTextTimeTutup));
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 100 && resultCode == RESULT_OK && data != null) {
             Uri fileUri = data.getData();
-            String fileName = getFileName(fileUri);  // fungsi untuk mendapatkan nama file
-            TextInputEditText etFile = findViewById(R.id.pilihFile);
-            etFile.setText(fileName);
+            String fileName = getFileName(fileUri);
+            pilihFile.setText(fileName);
         }
     }
 
-    public String getFileName(Uri uri) {
+    private String getFileName(Uri uri) {
         String result = null;
         if ("content".equals(uri.getScheme())) {
-            Cursor cursor = null;
-            try {
-                cursor = getContentResolver().query(uri, null, null, null, null);
+            try (Cursor cursor = getContentResolver().query(uri, null, null, null, null)) {
                 if (cursor != null && cursor.moveToFirst()) {
                     int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
-                    if (nameIndex != -1) {  // <-- Cek di sini
+                    if (nameIndex != -1) {
                         result = cursor.getString(nameIndex);
                     }
                 }
-            } finally {
-                if (cursor != null) cursor.close();
             }
         }
         if (result == null) {
